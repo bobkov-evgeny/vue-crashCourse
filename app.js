91b836626a1ed3db76c388405e52d48f2eb902db
Evgeny_Bobkov <freeedayy@gmail.com>
2021-07-01 11:52:11 +1000

    vue note list is done

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ const App = {
 	data() {
 		return {
 			counter: 0,
+			title: "Список заметок",
+			placeholderString: "Введите заметку",
+			inputValue: "",
+			notes: [],
 		};
+	},
+	methods: {
+		addInput(e) {
+			this.inputValue = e.target.value;
+		},
+		addNewNote() {
+			this.notes.push(this.inputValue);
+			this.inputValue = "";
+		},
+		deleteNote(index) {
+			this.notes.splice(index, 1);
+		},
 	},
 };
 
